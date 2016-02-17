645de75d075d04dc412b08774d4389c5a3e131b8
misterT2525 <info@mistertgroup.net>
2016-02-17 12:32:05 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HeadCollectionsFetcher
+
+Program for fetch heads from [heads.freshcoal.com](http://heads.freshcoal.com/) for [HeadDictionary](https://github.com/misterT2525/HeadDictionary/).
---
+++ src/main/java/net/mistertgroup/heads_collections_fetcher/Main.java
@@ -0,0 +1,15 @@
+package net.mistertgroup.heads_collections_fetcher;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author misterT2525
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
