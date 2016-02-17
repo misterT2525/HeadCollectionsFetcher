2b947b62f965c124b8ffe04aac728032ea5a7e11
misterT2525 <info@mistertgroup.net>
2016-02-17 13:58:06 -0800

    Implement Application

--- src/main/java/net/mistertgroup/heads_collections_fetcher/Main.java
+++ src/main/java/net/mistertgroup/heads_collections_fetcher/Main.java
@@ -1,7 +1,17 @@
 package net.mistertgroup.heads_collections_fetcher;
 
+import net.mistertgroup.heads_collections_fetcher.data.Head;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collection;
 
 /**
  * @author misterT2525
@@ -9,7 +19,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Main {
 
-    public static void main(String[] args) {
+    private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static File outputDir = new File("dist");
 
+    public static void main(String[] args) throws IOException {
+        MainCollection.fetch(gson).asMap().forEach(Main::saveCollection);
+    }
+
+    private static void saveCollection(@NonNull String name, Collection<Head> heads) {
+        if (!outputDir.exists() && !outputDir.mkdirs()) {
+            throw new IllegalStateException("Failed to create dist directory");
+        }
+
+        File file = new File(outputDir, name + ".json");
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(gson.toJson(heads));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
